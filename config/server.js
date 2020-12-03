603c96d0bc841091fc9d06dc1e9e06de8d94777e
Dimitris Trechas <dimitristrechas@gmail.com>
2020-12-03 20:23:31 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '826466dc4c9c93140c1658252cb80fe0'),
+    },
+  },
+});
