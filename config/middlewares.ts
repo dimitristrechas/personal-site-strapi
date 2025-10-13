7c82f4a1f074fca00698f2a6d258450d3497996c
Dimitris Trechas <dimitristrechas@gmail.com>
2025-10-13 13:14:58 +0300

    chore: update favicon

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,17 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  {
+    name: "strapi::favicon",
+    config: {
+      path: "./public/uploads/favicon.ico",
+    },
+  },
+  "strapi::public",
 ];
