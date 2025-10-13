501c981bd573681349f74dc41e732d18a9121f53
Dimitris Trechas <dimitristrechas@gmail.com>
2025-10-13 19:29:14 +0300

    Revert "fix: attempt revert of admin login hack"
    This reverts commit 23b68420922536e7f75b7fbe00ca91ce3fe6f339.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
-
+  register({ strapi }) {
+    // Force the socket to be treated as encrypted for proxy setups
+    strapi.server.use(async (ctx, next) => {
+      if (ctx.req?.socket) {
+        (ctx.req.socket as any).encrypted = true;
+      }
+      await next();
+    });
+  },
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
